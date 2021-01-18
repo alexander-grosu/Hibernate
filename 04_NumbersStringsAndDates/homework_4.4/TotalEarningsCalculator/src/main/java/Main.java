93a2a2cb7cff274296b795508e045246db38fa96
alexandr_grosu <alexander.grsu@gmail.com>
2021-01-18 12:10:59 +0300

    total earning calculator

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -5,14 +5,21 @@ public class Main {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         //TODO: напишите ваш код, результат вывести в консоль
 
-        String[] parts = text.split("\\s");
-        String sum1 = parts[2];
-        String sum2 = parts[6];
-        String sum3 = parts[11];
+        int a = text.indexOf('5');
+        int b = text.indexOf('5') + 4;
+        String num1 = text.substring(a,b);
 
-        int totalSum = Integer.parseInt(sum1) + Integer.parseInt(sum2) + Integer.parseInt(sum3);
+        int c = text.indexOf('7');
+        int d = text.indexOf('3') + 1;
+        String num2 = text.substring(c,d);
+
+        int e = text.lastIndexOf('3');
+        int f = text.lastIndexOf('0') + 1;
+        String num3 = text.substring(e,f);
+
+
+        int totalSum = Integer.parseInt(num1) + Integer.parseInt(num2) + Integer.parseInt(num3);
 
         System.out.println(totalSum);
     }
-
 }
