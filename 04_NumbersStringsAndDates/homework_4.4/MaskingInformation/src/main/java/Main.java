fbd4eb7a3f79a89e814a613e5adc9abb6c4acc7c
alexandr_grosu <alexander.grsu@gmail.com>
2021-01-20 16:29:43 +0300

    4/4/4

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -2,12 +2,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-
     }
-        public static String searchAndReplaceDiamonds (String text, String holder){
-            // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
 
-            return text.replaceAll("<[0-9].+?>", holder);
+    public static String searchAndReplaceDiamonds(String text, String holder) {
+        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+
+        int x = text.indexOf('>');
+        int y = text.lastIndexOf('>');
+
+        if (text.length() == 14) {
+            return "Пин код " + holder;
         }
+        if (text.length() == 29) {
+            return "Номер паспорта " + holder + text.substring(x + 1);
+        }
+        if (text.length() == 45) {
+            return "Номер кредитной карты " + holder + " " + text.substring(x + 2, x + 6) + " " + holder + " " + text.substring(y + 2);
+        }
+        if (text.length() == 43) {
+            return "Номер кредитной карты " + holder + " " + text.substring(x + 2);
+        } else {
 
+            return text;
+        }
     }
+}
