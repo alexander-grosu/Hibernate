92e4cf9aea0e597a01eb58e8b6acc8e148eedfdb
alexandr_grosu <alexander.grsu@gmail.com>
2021-01-26 12:19:53 +0300

    homework 4.5\salary total

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        String salary;
+        salary = "Коля заработал 50000 рублей, Федя - 45800 рубля, а Саша - 23000 рублей";
 
+        System.out.println("Сумма заработка друзей: " + calculateSalarySum(salary) + " руб.");
     }
 
     public static int calculateSalarySum(String text) {
@@ -9,6 +12,7 @@ public class Main {
         int sum = 0;
         String[] sumText = text.split(",");
         for (int i = 0; i < sumText.length; i++) {
+
             String sumReplace = sumText[i].replaceAll("[^0-9]", "").trim();
             sum += Integer.parseInt(sumReplace);
         }
