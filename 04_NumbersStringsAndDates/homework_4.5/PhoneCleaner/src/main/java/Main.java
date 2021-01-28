08058bcee7460d02010387e2f81df9213f851cf3
alexandr_grosu <alexander.grsu@gmail.com>
2021-01-29 01:53:57 +0300

    phone

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -9,10 +9,10 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        String regex = "(^(\\+7|8{1})?(\\s?\\(?\\d{3}\\)?\\s|\\s\\d{3}\\s|\\(\\d{3}\\)|\\d{3}|\\-?\\d{3}\\-)(\\d{7}|\\d{3}\\-\\d{2}\\-\\d{2})$)";
+        String regex = "(^(\\+?[7,8]{1}?(\\s?\\(?\\d{3}\\)?\\s|\\s\\d{3}\\s|\\(\\d{3}\\)|\\d{3}|\\-?\\d{3}\\-)(\\d{7}|\\d{3}\\-\\d{2}\\-\\d{2}))$)";
         Matcher m = Pattern.compile(regex).matcher(input);
         if (m.matches() && input.replaceAll("\\D","").length() == 11) {
-            System.out.println(input.replaceAll("(8){1}", "7").replaceAll("\\D", ""));
+            System.out.println(input.replaceAll("[8]{1}", "7").replaceAll("\\D", ""));
         } else if (m.matches() && input.replaceAll("\\D","").length() == 10){
             System.out.println("7" + input.replaceAll("\\D",""));
         }
