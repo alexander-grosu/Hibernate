0d09e606ceb1096bb0360e80f85f6a596312d6c4
alexandr_grosu <alexander.grsu@gmail.com>
2021-02-02 01:43:01 +0300

    calendar

--- 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
@@ -10,9 +10,7 @@ public class Main {
         int day = 31;
         int month = 0;
         int year = 1990;
-
         System.out.println(collectBirthdays(year, month, day));
-
     }
 
     public static String collectBirthdays(int year, int month, int day) {
@@ -21,17 +19,18 @@ public class Main {
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
         int i = 0;
+        month--;
         Calendar bDay = new GregorianCalendar(year, month, day);
         Calendar today = GregorianCalendar.getInstance();
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy - EEE", Locale.ENGLISH);
-        System.out.println(i + " - " + df.format(bDay.getTime()));
 
+        StringBuilder stringBuilder = new StringBuilder();
         while (bDay.getTime().before(today.getTime())) {
+            stringBuilder.append(i).append(" - ").append(df.format(bDay.getTime())).append("\r\n");
             bDay.add(Calendar.YEAR, 1);
             i++;
-            System.out.println(i + " - " + df.format(bDay.getTime()));
         }
-        return "";
+        return stringBuilder.toString();
     }
 }
 
