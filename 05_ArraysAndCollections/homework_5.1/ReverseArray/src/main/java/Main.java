7f4a5fb226d0c497047e3ca49b8210f5811dcecd
alexandr_grosu <alexander.grsu@gmail.com>
2021-02-03 22:36:56 +0300

    hw 5.1

--- 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,15 @@
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-        String line = "Каждый охотник желает знать, где сидит фазан";
+
+
+        String line[] = {"Каждый", "охотник", "желает", "знать", "где", "сидит", "фазан"};
+        String str = "Samuel Leroy Jackson";
+        String[] massive = str.split("\\s+");
+
+        System.out.println(Arrays.toString(ReverseArray.reverse((line))));
+        System.out.println(Arrays.toString(ReverseArray.reverse((massive))));
+
     }
 }
--- 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/ReverseArray.java
+++ 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,7 +1,12 @@
 public class ReverseArray {
 
-    //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
-    public static String[] reverse (String[] strings){
+    public static String[] reverse(String[] strings) {
+        String temp;
+        for (int i = 0; i < strings.length / 2; i++) {
+            temp = strings[strings.length - i - 1];
+            strings[strings.length - i - 1] = strings[i];
+            strings[i] = temp;
+        }
         return strings;
     }
 }
