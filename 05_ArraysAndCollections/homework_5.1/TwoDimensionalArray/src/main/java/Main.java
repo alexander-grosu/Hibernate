6d7fd84e092cda7ea27a1004f4c54ee3770b2fb1
alexandr_grosu <alexander.grsu@gmail.com>
2021-02-11 04:18:22 +0300

    work 5.1 diagonale

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
-        int [][] array = new int[7][7];
-        System.out.println(Arrays.deepToString(TwoDimensionalArray.getTwoDimensionalArray(array.length)));
+        System.out.println(Arrays.deepToString((TwoDimensionalArray.getTwoDimensionalArray(6))));
     }
 }
--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -2,25 +2,26 @@ public class TwoDimensionalArray {
     public static char symbol = 'X';
 
     public static char[][] getTwoDimensionalArray(int size) {
-
         //TODO: Написать метод, который создаст двумерный массив char заданного размера.
         // массив должен содержать символ symbol по диагоналям, пример для size = 3
         // [X,  , X]
         // [ , X,  ]
         // [X,  , X]
         char [][] chessBoard = new char[size][size];
-        for(int i = 0; i < size; i++) {
-            if(i == size-1-i) chessBoard[j] = symbol; {
-                System.out.print(symbol);
-                continue;
+
+        for (int a =0; a < chessBoard.length; a++){
+            for (int b =0; b < chessBoard[0].length;b++){
+                if( a == b){
+                    chessBoard[a][b] = symbol;
+                }
+                else if (a == chessBoard[0].length-1 - b){
+                    chessBoard[a][b] = symbol;
+                } else {
+                    chessBoard[a][b] = ' ';
+                }
             }
-            int temp = chessBoard[i][i];
-            chessBoard[i][i] = chessBoard[i][size-1-i];
-            chessBoard[i][size-1-i] = (char) temp;
-            System.out.println();
         }
-
-       return chessBoard;
+        System.out.println();
+        return chessBoard;
     }
-
 }
