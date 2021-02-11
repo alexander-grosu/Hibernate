6004f9545aa0f38a3094a5b53be482ef5396df0c
alexandr_grosu <alexander.grsu@gmail.com>
2021-02-11 04:50:46 +0300

    5.1 array[][]

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
-        System.out.println(Arrays.deepToString((TwoDimensionalArray.getTwoDimensionalArray(6))));
+        char[][] array = new char[5][5];
+        System.out.println(Arrays.deepToString(TwoDimensionalArray.getTwoDimensionalArray(array.length)));
     }
 }
