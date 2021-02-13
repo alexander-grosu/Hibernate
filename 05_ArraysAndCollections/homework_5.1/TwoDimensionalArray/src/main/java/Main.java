12d4f9748223e71a9feb14a830d3aa94a29f0a4a
alexandr_grosu <alexander.grsu@gmail.com>
2021-02-13 10:01:51 +0300

    xDiagonale

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
-        char[][] array = new char[12][10];
-        System.out.println(Arrays.deepToString(TwoDimensionalArray.getTwoDimensionalArray(array.length)).replaceAll("\\]\\,\\s\\[", "\n").replaceAll("[^X\\s]", ""));
-        System.out.println(Arrays.deepToString(TwoDimensionalArray.getTwoDimensionalArray(array.length)));
+        char [][] array = new char[12][10];
+        String xDiagonale = Arrays.deepToString(TwoDimensionalArray.getTwoDimensionalArray(array.length)).replaceAll("\\]\\,\\s\\[", "\n").replaceAll("[^X\\s]", "");
+        System.out.println(xDiagonale);
     }
 
 }
