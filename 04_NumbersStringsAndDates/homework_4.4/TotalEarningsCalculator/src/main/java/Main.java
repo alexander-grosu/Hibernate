85d49939fd5fbc12d2003118d5660dd2a8ae619f
alexandr_grosu <alexander.grsu@gmail.com>
2021-03-21 18:50:36 +0300

    num sum index

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,20 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        //TODO: напишите ваш код, результат вывести в консоль
 
         int a = text.indexOf('5');
-        int b = text.indexOf('5') + 4;
+        int b = a + 4;
         String num1 = text.substring(a,b);
 
         int c = text.indexOf('7');
-        int d = text.indexOf('3') + 1;
+        int d = c + 4;
         String num2 = text.substring(c,d);
 
         int e = text.lastIndexOf('3');
         int f = text.lastIndexOf('0') + 1;
         String num3 = text.substring(e,f);
-
 
         int totalSum = Integer.parseInt(num1) + Integer.parseInt(num2) + Integer.parseInt(num3);
 
