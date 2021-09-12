ba57a451a795a6eea3c1109170d07d0b103fc958
unknown <alexander.grsu@gmail.com>
2021-09-12 16:37:00 +0300

    homework 11.1 11.2

--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
         Path folder2 = Paths.get("D:\\Java_Home_Work\\java_basics\\05_ArraysAndCollections");
         Path folder3 = Paths.get("src/main/java");
         Path folder4 = Paths.get("src");
+        Path folder5 = Paths.get("D:/Java HomeWorkNew/java_basics/11_FilesAndNetwork/files/map.json");
+        Path folder6 = Paths.get("D:/Java HomeWorkNew/java_basics/11_FilesAndNetwork/files/movementList.csv");
 
         System.out.println("Folder '" + folder1.getFileName() + "' size: " + FileUtils
                 .calculateFolderSize(folder1.toAbsolutePath().toString()) / 1024 + " kb");
@@ -21,6 +23,10 @@ public class Main {
 
         System.out.println("Folder '" + folder4.getFileName() + "' size: " + FileUtils
                 .calculateFolderSize(folder4.toAbsolutePath().toString()) / 1024 + " kb");
+        System.out.println("Folder '" + folder5.getFileName() + "' size: " + FileUtils
+                .calculateFolderSize(folder5.toAbsolutePath().toString()) / 1024 + " kb");
+        System.out.println("Folder '" + folder6.getFileName() + "' size: " + FileUtils
+                .calculateFolderSize(folder6.toAbsolutePath().toString()) / 1024 + " kb");
 
     }
 }
