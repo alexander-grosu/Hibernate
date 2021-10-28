5a0fd89c852c7271b1e528d679a661c900ecf03e
unknown <alexander.grsu@gmail.com>
2021-10-28 20:15:01 +0300

    io/nio

--- 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
+++ 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
@@ -21,19 +21,41 @@ public class Movements {
             }
             for (int i = 1; i < incomeExpenseList.size(); i++){
                 if (incomeExpenseList.get(i).split(",").length == 8) {
+
+                    //if income = String int && expense = String int
                     expense += Double.parseDouble(incomeExpenseList.get(i).split(",")[7]);
                     income += Double.parseDouble((incomeExpenseList.get(i).split(",")[6]));
+
                 }
+
                 if (incomeExpenseList.get(i).split(",").length == 9) {
-                    expense += Double.parseDouble((incomeExpenseList.get(i).split(",")[7].concat(".")
-                            .concat(incomeExpenseList.get(i).split(",")[8])).replaceAll("\\\"", ""));
-                    income += Double.parseDouble((incomeExpenseList.get(i).split(",")[6]));
+
+                    if (incomeExpenseList.get(i).split(",")[incomeExpenseList.get(i).split(",").length-1].matches("[0-9]+\\\"")) {
+
+                        //if income = String int && expense = String "double"
+                        expense += Double.parseDouble(((incomeExpenseList.get(i).split(",")[7])
+                                .concat(".").concat(incomeExpenseList.get(i).split(",")[8])).replaceAll("\\\"",""));
+                        income += Double.parseDouble(incomeExpenseList.get(i).split(",")[6]);
+
+                    }
+                    else if(incomeExpenseList.get(i).split(",")[incomeExpenseList.get(i).split(",").length-1].matches("[0-9]+")){
+
+                        //if income = String "double" && expense = String int
+                        expense += Double.parseDouble((incomeExpenseList.get(i).split(",")[8]));
+                        income += Double.parseDouble((incomeExpenseList.get(i).split(",")[6].concat(".")
+                                .concat(incomeExpenseList.get(i).split(",")[7])).replaceAll("\\\"",""));
+
+                    }
                 }
+                if(incomeExpenseList.get(i).split(",").length == 10) {
 
-
+                    // if income = String "double" && expense = String "double"
+                    expense += Double.parseDouble((incomeExpenseList.get(i).split(",")[8]
+                            .concat(".").concat(incomeExpenseList.get(i).split(",")[9])).replaceAll("\\\"",""));
+                    income += Double.parseDouble(((incomeExpenseList.get(i).split(",")[6])
+                            .concat(".").concat(incomeExpenseList.get(i).split(",")[7])).replaceAll("\\\"",""));
+                }
             }
-            System.out.println(income);
-            System.out.println(expense);
 
         } catch (IOException
                 | RuntimeException e) {
