8b1890692eb64d5186b32fb863d6810f308f161e
unknown <alexander.grsu@gmail.com>
2021-11-18 15:14:22 +0200

    jsoup URL parseHtml download image from webSite

--- 11_FilesAndNetwork/homework_11.4/src/Main.java
+++ 11_FilesAndNetwork/homework_11.4/src/Main.java
@@ -2,6 +2,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -17,11 +23,30 @@ public class Main {
 
             for (Element img : elements) {
 
-                String image = img.attr("src");
+                String image = img.attr("abs:src");
                 String imageName = image.substring(image.lastIndexOf("/") + 1);
                 System.out.println("image name: " + imageName);
                 System.out.println("image link: " + image);
                 System.out.println();
+
+                BufferedImage bufferedImage = null;
+                try {
+
+                    URL url = new URL(image);
+                    // read the url
+                    bufferedImage = ImageIO.read(url);
+
+                    String destinationFolder = "D:/images/" + imageName;
+
+                    //for png
+                    ImageIO.write(bufferedImage, "png", new File(destinationFolder));
+
+                    // for jpg
+                    ImageIO.write(bufferedImage, "jpg", new File(destinationFolder));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
         } catch (Exception e) {
