16c4f7a00b231228d8af83a4bdbde748fb28b559
unknown <alexander.grsu@gmail.com>
2022-01-04 08:38:37 +0200

    hibernate pojo

---
+++ Hibernate/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+        List courseList = session.createQuery("FROM " + Course.class.getSimpleName()).getResultList();
+
+        int size = courseList.size();
+        int id = 1;
+
+        Course course = session.get(Course.class, id);
+        System.out.println("\n" + course.getName() + " | " + course.getType() + "\nstudents count: " + course.getStudentsCount() + "\n");
+        while (id < size) {
+            id++;
+            course = session.get(Course.class, id);
+            System.out.println(course.getName() + " | " + course.getType() +  "\nstudents count: " + course.getStudentsCount() + "\n");
+        }
+    }
+}
