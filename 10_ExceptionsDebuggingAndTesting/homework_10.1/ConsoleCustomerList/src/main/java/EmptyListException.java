87279d2b6f1af32e75761f2711396c491db4571f
Alexander Grosu <74964514+alexander-grosu@users.noreply.github.com>
2022-02-21 19:04:53 +0200

    Delete 10_ExceptionsDebuggingAndTesting directory

--- 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/EmptyListException.java
+++
@@ -1,6 +0,0 @@
-public class EmptyListException extends RuntimeException {
-    @Override
-    public String getMessage() {
-        return "Empty list. Couldn't find customers.";
-    }
-}
--- 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/ThisCustomerNotFoundOnTheListException.java
+++
@@ -1,7 +0,0 @@
-public class ThisCustomerNotFoundOnTheListException extends RuntimeException {
-
-    @Override
-    public String getMessage() {
-        return "Error. Couldn't find customer.";
-    }
-}
