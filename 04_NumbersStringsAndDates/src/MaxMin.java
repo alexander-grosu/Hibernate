28eb8d3f943ed7fad774689c12ab1dc0d7b27e35
Alexander Grosu <74964514+alexander-grosu@users.noreply.github.com>
2022-02-21 19:03:03 +0200

    Delete 04_NumbersStringsAndDates directory

--- 04_NumbersStringsAndDates/src/MaxMin.java
+++
@@ -1,6 +0,0 @@
-public class MaxMin {
-    public static void main (String [] args){
-        char count;       System.out.println();
-    }
-
-}
--- 04_NumbersStringsAndDates/untitled/src/Cicle.java
+++
@@ -1,9 +0,0 @@
-public class Cicle {
-    public static void main(String[] args) {
-
-        for (int s = 0; s <= 20; ++s) {
-            System.out.print(s + " ");
-        }
-
-    }
-}
